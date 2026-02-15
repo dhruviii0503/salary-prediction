d2762b5bada41411265f91a7cdf649d87b6e08cd
Dhruvi Parmar <dhruviii0503@gmail.com>
2026-02-15 14:22:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,12 +34,14 @@ df = pd.DataFrame({
 })
 
 
-if st.button("Predict"):
-  for col in encoder:
-    df[col] = encoder[col].transform(df[col])
+if st.button("Predict Salary"):
+ for col in encoder:
+        df[col] = encoder[col].transform(df[col])
 
-    prediction = model.predict(df)
-    st.success(f"Predicted salary: {prediction[0]:,.2f}")
+     df = df[model.feature_names_in_]
+     df = df.astype(float)
+     prediction = model.predict(df)
+     st.success(f"Predicted Salary: ₹ {prediction[0]:,.2f}")
 
 
 
